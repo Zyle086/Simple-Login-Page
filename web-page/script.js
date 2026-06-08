16803aa9227cbe78eb56411b3a0233dc41982e27
Zyle086 <nuerruzzle@gmail.com>
2026-06-08 17:53:08 -0400

    Img slider

--- web-page/script.js
+++ web-page/script.js
@@ -1,1 +1,15 @@
 
+$("#to-signup").on("click", function(e){
+    $(".img-container").removeClass("right");
+    $(".img-container").addClass("left");
+    $(".login-container").addClass("hidden");
+    $(".signup-container").removeClass("hidden");
+}); 
+
+
+$("#to-login").on("click", function(e){
+    $(".img-container").removeClass("left");
+    $(".img-container").addClass("right");
+    $(".signup-container").addClass("hidden");
+    $(".login-container").removeClass("hidden");
+}); 
