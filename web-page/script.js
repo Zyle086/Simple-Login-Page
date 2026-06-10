5274fd278afa9e9ff91f7807f51aeb288516cbf5
Zyle086 <nuerruzzle@gmail.com>
2026-06-10 12:38:47 -0400

    Semi Finished Animation

--- web-page/script.js
+++ web-page/script.js
@@ -1,15 +1,26 @@
+const DELAY = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+// Signup Form
 $("#to-signup").on("click", function(e){
     $(".img-container").removeClass("right");
     $(".img-container").addClass("left");
-    $(".login-container").addClass("hidden");
+    $(".login-container").addClass("disappearLeft");
     $(".signup-container").removeClass("hidden");
+    setTimeout(() => {
+        $(".login-container").addClass("hidden");
+        $(".login-container").removeClass("disappearLeft");
+    }, 500);
 }); 
 
 
+// Login Form
 $("#to-login").on("click", function(e){
     $(".img-container").removeClass("left");
     $(".img-container").addClass("right");
-    $(".signup-container").addClass("hidden");
+    $(".signup-container").addClass("disappearRight");
     $(".login-container").removeClass("hidden");
+    setTimeout(() => {
+        $(".signup-container").addClass("hidden");
+        $(".signup-container").removeClass("disappearRight");
+    }, 500);
 }); 
